de80b962b9f045fc49d454090db94c8ace5b7eb7
Samuel Cartwright <sam.cartwright123@hotmail.co.uk>
2021-02-21 10:10:58 +0100

    part 1

---
+++ part1/game/game.py
@@ -0,0 +1,53 @@
+import pygame as pg
+import sys
+from .world import World
+from .settings import TILE_SIZE
+
+
+class Game:
+
+    def __init__(self, screen, clock):
+        self.screen = screen
+        self.clock = clock
+        self.width, self.height = self.screen.get_size()
+
+        self.world = World(10, 10, self.width, self.height)
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(60)
+            self.events()
+            self.update()
+            self.draw()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+                    sys.exit()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+
+        for x in range(self.world.grid_length_x):
+            for y in range(self.world.grid_length_y):
+
+                sq = self.world.world[x][y]["cart_rect"]
+                rect = pg.Rect(sq[0][0], sq[0][1], TILE_SIZE, TILE_SIZE)
+                pg.draw.rect(self.screen, (0, 0, 255), rect, 1)
+
+                p = self.world.world[x][y]["iso_poly"]
+                p = [(x + self.width/2, y + self.height/4) for x, y in p]
+                pg.draw.polygon(self.screen, (255, 0, 0), p, 1)
+
+
+        pg.display.flip()
+
---
+++ part1/game/world.py
@@ -0,0 +1,53 @@
+
+import pygame as pg
+from .settings import TILE_SIZE
+
+
+
+class World:
+
+    def __init__(self, grid_length_x, grid_length_y, width, height):
+        self.grid_length_x = grid_length_x
+        self.grid_length_y = grid_length_y
+        self.width = width
+        self.height = height
+
+        self.world = self.create_world()
+
+    def create_world(self):
+
+        world = []
+
+        for grid_x in range(self.grid_length_x):
+            world.append([])
+            for grid_y in range(self.grid_length_y):
+                world_tile = self.grid_to_world(grid_x, grid_y)
+                world[grid_x].append(world_tile)
+
+        return world
+
+    def grid_to_world(self, grid_x, grid_y):
+
+        rect = [
+            (grid_x * TILE_SIZE, grid_y * TILE_SIZE),
+            (grid_x * TILE_SIZE + TILE_SIZE, grid_y * TILE_SIZE),
+            (grid_x * TILE_SIZE + TILE_SIZE, grid_y * TILE_SIZE + TILE_SIZE),
+            (grid_x * TILE_SIZE, grid_y * TILE_SIZE + TILE_SIZE)
+        ]
+
+        iso_poly = [self.cart_to_iso(x, y) for x, y in rect]
+
+        out = {
+            "grid": [grid_x, grid_y],
+            "cart_rect": rect,
+            "iso_poly": iso_poly
+        }
+
+        return out
+
+    def cart_to_iso(self, x, y):
+        iso_x = x - y
+        iso_y = (x + y)/2
+        return iso_x, iso_y
+
+
